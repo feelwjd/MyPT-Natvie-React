7f4e01e6b1281851a08123cef645ed727f2caf25
feelwjd <feelwjd@gmail.com>
2022-09-21 17:46:21 +0900

    feat : session Login

--- routes/Screen/splashScreen.js
+++ routes/Screen/splashScreen.js
@@ -5,6 +5,7 @@ import {
   StyleSheet,
   Image
 } from 'react-native';
+import { getItemFromAsync, setItemToAsync } from '../module/getItem';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import ServerConnect from "../module/ServerConnect"
 
@@ -17,26 +18,49 @@ const SplashScreen = ({navigation}) => {
       //Check if user_id is set or not
       //If not then send for Authentication
       //else send to Home Screen
-      AsyncStorage.getItem('email').then(async function(value){
-        console.log(value);
-        ServerConnect('api/SessionCheck',{'cookie':value} ,async function(err, result){
-          if (err){
-            console.log(err);
-            await AsyncStorage.clear();
+      var nav = 0;
+      var sessionCode = getItemFromAsync('email');
+      sessionCode.then(result => ServerConnect('session/SessionCheck',{'cookie':result},function(err,result){
+        if(err){
+          console.log(err);
+          AsyncStorage.clear()
+        }else{
+          if(result.status === 201){
+            console.log("세션 확인 되었습니다.");
+            nav = 1;
           }else{
-            if(result.status === 201){
-              console.log("세션 확인 되었습니다.")
-            }else{
-              console.log(result.status);
-              console.log(result.msg);
-              await AsyncStorage.clear();
-            }
+            console.log(result.status , result.msg);
+            AsyncStorage.clear()
           }
-        });
+        }
+      })).then(
         navigation.replace(
-          value === null ? 'Auth' : 'Main'
-        );
-      });
+          nav === 0 ? 'Auth' : 'Main'
+          )
+      )
+
+      // AsyncStorage.getItem('email').then(async function(value){
+      //   console.log(value);
+      //   ServerConnect('session/SessionCheck',{'cookie':value} ,async function(err, result){
+      //     if (err){
+      //       console.log(err);
+      //       await AsyncStorage.clear().then(async function(){
+      //         navigation.replace('Auth');
+      //       });
+      //     }else{
+      //       if(result.status === 201){
+      //         console.log("세션 확인 되었습니다.");
+      //         navigation.replace('Main');
+      //       }else{
+      //         console.log(result.status);
+      //         console.log(result.msg);
+      //         await AsyncStorage.clear().then(async function(){
+      //           navigation.replace('Auth');
+      //         });
+      //       }
+      //     }
+      //   });
+      // });
     }, 1000);
   }, []);
 
